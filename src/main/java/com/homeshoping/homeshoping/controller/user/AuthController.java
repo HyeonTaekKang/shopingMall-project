2065d243bd1362a0063b0fdcbfe4e7e5cdb547d6
Kangteak <wjavmtngkr1@naver.com>
2022-12-31 04:25:53 +0900

    로그인을 위한 AUthController

--- src/main/java/com/homeshoping/homeshoping/controller/user/AuthController.java
+++ src/main/java/com/homeshoping/homeshoping/controller/user/AuthController.java
@@ -1,18 +1,20 @@
 package com.homeshoping.homeshoping.controller.user;
 
-import com.homeshoping.homeshoping.request.user.Login;
+import com.homeshoping.homeshoping.request.member.Login;
+import com.homeshoping.homeshoping.service.member.AuthService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final AuthService authService;
 
     @PostMapping("/auth/login")
     public void login(@RequestBody Login login){
-        // json 아이디 / 비번
-        // DB에서 조회
-        // 토큰을 응답
+        authService.signIn(login);
     }
-
 }
