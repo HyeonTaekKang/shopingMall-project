a33a5220716f60c4669c4c1963187aa08c26856c
Kangteak <wjavmtngkr1@naver.com>
2023-01-03 03:45:39 +0900

    생성자로 Order객체를 생성한 것을 -> 메서드를 통해서 Order 객체를 생성하는 것으로 변경함.

--- src/main/java/com/homeshoping/homeshoping/entity/order/Order.java
+++ src/main/java/com/homeshoping/homeshoping/entity/order/Order.java
@@ -2,11 +2,15 @@ package com.homeshoping.homeshoping.entity.order;
 
 import com.homeshoping.homeshoping.entity.orderItem.OrderItem;
 import com.homeshoping.homeshoping.entity.member.Member;
+import com.homeshoping.homeshoping.request.member.MemberCreate;
+import com.homeshoping.homeshoping.request.order.OrderCreate;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,12 +25,39 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY , cascade = CascadeType.ALL)
     @JoinColumn(name = "member_id")
-    private Member member;
+    private Member member;  // 누가 주문했는지
 
     @OneToMany(mappedBy = "order")
-    private List<OrderItem> orderItems = new ArrayList<>();
+    private List<OrderItem> orderItems = new ArrayList<>(); // 주문한 아이템들은 뭔지
+
+    private LocalDateTime orderDate; // 언제 주문했는지
+
+    @Builder
+    public Order(Long id, Member member, List<OrderItem> orderItems, LocalDateTime orderDate) {
+        this.id = id;
+        this.member = member;
+        this.orderItems = orderItems;
+        this.orderDate = orderDate;
+    }
+
+    //생성자 오버로딩 ( Order 추가 (OrderCreate -> Order) )
+//    @Builder
+//    public Order(OrderCreate orderCreate) {
+//        // 주문한 회원의 정보중 이름만 전달
+//        member = orderCreate.getMemberCreate()
+//
+//    }
+
+    // Order 생성 메서드
+    public static Order createOrder(Member member){
+
+        Order order = new Order();
+        order.setMember(member);
+
+        return order;
+    }
 
     // 연관관계 편의메서드 ( member 셋팅 )
     public void setMember(Member member){
@@ -37,10 +68,13 @@ public class Order {
     public void addOrderItems(OrderItem orderItem){
         orderItems.add(orderItem);
 //        orderItem.setOrder(this);
+
         OrderItem.builder()
                 .order(this)
                 .build();
     }
+
+
 
 
 
