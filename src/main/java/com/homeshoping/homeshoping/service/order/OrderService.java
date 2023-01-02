746c6ad6b0925e426bb373fa5b0020c8cdb8a25b
Kangteak <wjavmtngkr1@naver.com>
2023-01-03 03:47:22 +0900

    주문한 회원의 정보를 받아서, 회원의 정보랑 Order를 같이 save 하는 create 메서드를 제작함.

--- src/main/java/com/homeshoping/homeshoping/service/order/OrderService.java
+++ src/main/java/com/homeshoping/homeshoping/service/order/OrderService.java
@@ -1,13 +1,36 @@
 package com.homeshoping.homeshoping.service.order;
 
+import com.homeshoping.homeshoping.Exception.MemberNotFound;
+import com.homeshoping.homeshoping.entity.member.Member;
+import com.homeshoping.homeshoping.entity.order.Order;
+import com.homeshoping.homeshoping.repository.member.MemberRepository;
+import com.homeshoping.homeshoping.repository.order.OrderRepository;
+import com.homeshoping.homeshoping.request.member.MemberCreate;
+import com.homeshoping.homeshoping.request.order.OrderCreate;
+import com.homeshoping.homeshoping.response.order.OrderResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class OrderService {
 
-    private void createOrder(){}
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
 
-    private OrderResponse getOrders(){}
+    @Transactional
+    public void create(Long memberId){
+
+        // 회원정보
+        Member member = memberRepository.findById(memberId).orElseThrow(() -> new MemberNotFound());
+
+        // order 생성
+        Order order = Order.createOrder(member);
+
+        // 생성한 order 저장
+        orderRepository.save(order);
+    }
+
 }
