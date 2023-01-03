9ca4e65d27ddc81e6b3f001c5f22ad597dce2265
Kangteak <wjavmtngkr1@naver.com>
2023-01-04 00:12:14 +0900

    멤버변수인 'member'의 cascade = CascadeType.ALL 부분 삭제

--- src/main/java/com/homeshoping/homeshoping/entity/order/Order.java
+++ src/main/java/com/homeshoping/homeshoping/entity/order/Order.java
@@ -26,7 +26,7 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY , cascade = CascadeType.ALL)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;  // 누가 주문했는지
 
