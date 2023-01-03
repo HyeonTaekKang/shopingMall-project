af817d73cc4f24cd3e00c80dba06bf4d7be05da7
Kangteak <wjavmtngkr1@naver.com>
2023-01-03 18:26:56 +0900

    'createOrderItem' 메서드로 orderItem 을 생성한다음 , 생성한 orderItem을 주문 생성 메서드인 'createOrder' 의 메개변수로 넣어서 주문을 생성함.

--- src/main/java/com/homeshoping/homeshoping/service/order/OrderService.java
+++ src/main/java/com/homeshoping/homeshoping/service/order/OrderService.java
@@ -1,10 +1,15 @@
 package com.homeshoping.homeshoping.service.order;
 
+import com.homeshoping.homeshoping.Exception.ItemNotFound;
 import com.homeshoping.homeshoping.Exception.MemberNotFound;
+import com.homeshoping.homeshoping.entity.Item.Item;
 import com.homeshoping.homeshoping.entity.member.Member;
 import com.homeshoping.homeshoping.entity.order.Order;
+import com.homeshoping.homeshoping.entity.orderItem.OrderItem;
+import com.homeshoping.homeshoping.repository.Item.ItemRepository;
 import com.homeshoping.homeshoping.repository.member.MemberRepository;
 import com.homeshoping.homeshoping.repository.order.OrderRepository;
+import com.homeshoping.homeshoping.repository.orderItem.OrderItemRepository;
 import com.homeshoping.homeshoping.request.member.MemberCreate;
 import com.homeshoping.homeshoping.request.order.OrderCreate;
 import com.homeshoping.homeshoping.response.order.OrderResponse;
@@ -19,15 +24,23 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final MemberRepository memberRepository;
+    private final OrderItemRepository orderItemRepository;
+    private final ItemRepository itemRepository;
 
-    @Transactional
-    public void create(Long memberId){
+    @Transactional      // memberId    // itemId    // 주문 수량
+    public void create(Long memberId, Long itemId , int orderCount){
 
-        // 회원정보
-        Member member = memberRepository.findById(memberId).orElseThrow(() -> new MemberNotFound());
+        // 주문한 회원의 정보 찾아오기 ( member )
+        Member member = memberRepository.findById(memberId).orElseThrow(MemberNotFound::new);
 
-        // order 생성
-        Order order = Order.createOrder(member);
+        // 회원이 주문한 아이템 찾아오기 ( item )
+        Item item = itemRepository.findById(itemId).orElseThrow(ItemNotFound::new);
+
+        // 회원이 주문한 아이템으로 주문아이템 생성 ( orderItem )
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), orderCount);
+
+        // 주문 생성 ( member + orderItem )
+        Order order = Order.createOrder(member , orderItem);
 
         // 생성한 order 저장
         orderRepository.save(order);
