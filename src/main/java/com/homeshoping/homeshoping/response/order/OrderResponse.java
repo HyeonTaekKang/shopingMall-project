ca6c4d6dd398adf011926475cf9a2b2329f4f6b1
Kangteak <wjavmtngkr1@naver.com>
2023-01-05 05:44:49 +0900

    Order에 대한 정보를 response 할때 사용할 DTO인 OrderResponse 클래스를 만들었음.

--- src/main/java/com/homeshoping/homeshoping/response/order/OrderResponse.java
+++ src/main/java/com/homeshoping/homeshoping/response/order/OrderResponse.java
@@ -1,6 +1,23 @@
 package com.homeshoping.homeshoping.response.order;
 
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+
 public class OrderResponse {
+    private Long orderItemId; // 주문한 아이템의 Id
+    private String orderItemName; // 주문한 아이템의 이름
+    private String orderPrice; // 주문한 아이템의 가격
+    private LocalDateTime orderDate; // 주문한 날짜
+    private String orderItemStockQuantity; // 주문한 아이템의 재고수량
 
-
+    @QueryProjection
+    public OrderResponse(Long orderItemId, String orderItemName, String orderPrice, LocalDateTime orderDate, String orderItemStockQuantity) {
+        this.orderItemId = orderItemId;
+        this.orderItemName = orderItemName;
+        this.orderPrice = orderPrice;
+        this.orderDate = orderDate;
+        this.orderItemStockQuantity = orderItemStockQuantity;
+    }
 }
