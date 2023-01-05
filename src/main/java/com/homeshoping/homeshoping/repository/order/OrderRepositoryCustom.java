7968f48a536fa8c1f60eed6973afcc885a1cbecd
Kangteak <wjavmtngkr1@naver.com>
2023-01-05 17:32:21 +0900

    fetch join을 사용하면 엔티티 형태로 반환해야지 DTO로는 반환하지 못한다는 것을 깨닫고 OrderItem 이라는 엔티티로 반환하게 코드를 변경

--- src/main/java/com/homeshoping/homeshoping/repository/order/OrderRepositoryCustom.java
+++ src/main/java/com/homeshoping/homeshoping/repository/order/OrderRepositoryCustom.java
@@ -1,10 +1,11 @@
 package com.homeshoping.homeshoping.repository.order;
 
 import com.homeshoping.homeshoping.entity.order.Order;
+import com.homeshoping.homeshoping.entity.orderItem.OrderItem;
 import com.homeshoping.homeshoping.response.order.OrderResponse;
 
 public interface OrderRepositoryCustom {
 
     // 해당 회원이 주문한 주문의 정보를 최신순으로 가져오는 메서드
-    public OrderResponse getOrder(Long memberId);
+    OrderItem getOrder(Long memberId);
 }
