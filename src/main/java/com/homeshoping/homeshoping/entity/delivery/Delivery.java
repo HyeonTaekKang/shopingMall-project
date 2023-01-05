f6d9b94471968b3974817d9d775b312f91933219
Kangteak <wjavmtngkr1@naver.com>
2023-01-06 05:06:05 +0900

    배달정보에 대한 정보를 저장하기 위해서 Delivery 엔티티를 생성하였음.

--- src/main/java/com/homeshoping/homeshoping/entity/delivery/Delivery.java
+++ src/main/java/com/homeshoping/homeshoping/entity/delivery/Delivery.java
@@ -1,4 +1,22 @@
 package com.homeshoping.homeshoping.entity.delivery;
 
+import com.homeshoping.homeshoping.entity.member.Address;
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
 public class Delivery {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus deliveryStatus; // [ READY , DELIVER ]
+
+    @Embedded
+    private Address address;
 }
