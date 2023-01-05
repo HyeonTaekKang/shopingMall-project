71cf8771e8f55603e79105852dd65ca471726d28
Kangteak <wjavmtngkr1@naver.com>
2023-01-05 20:22:37 +0900

    @QueryProjection을 없애고 , @Builder로 바꿈. 그리고 클래스에 orderCount(주문수량 ) 과 orderStatus(주문상태) 라는 멤버변수를 추가하였음.

--- src/main/java/com/homeshoping/homeshoping/response/order/OrderResponse.java
+++ src/main/java/com/homeshoping/homeshoping/response/order/OrderResponse.java
@@ -1,23 +1,30 @@
 package com.homeshoping.homeshoping.response.order;
 
+import com.homeshoping.homeshoping.entity.order.OrderStatus;
 import com.querydsl.core.annotations.QueryProjection;
 import lombok.Builder;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
+@Getter
 public class OrderResponse {
     private Long orderItemId; // 주문한 아이템의 Id
     private String orderItemName; // 주문한 아이템의 이름
-    private String orderPrice; // 주문한 아이템의 가격
+    private int orderPrice; // 주문한 아이템의 가격
+    private int orderCount; // 주문한 아이템의 갯수
     private LocalDateTime orderDate; // 주문한 날짜
-    private String orderItemStockQuantity; // 주문한 아이템의 재고수량
+    private int orderItemStockQuantity; // 주문한 아이템의 재고수량
+    private OrderStatus orderStatus; // 주문 상태 [ ORDER, CANCEL ]
 
-    @QueryProjection
-    public OrderResponse(Long orderItemId, String orderItemName, String orderPrice, LocalDateTime orderDate, String orderItemStockQuantity) {
+    @Builder
+    public OrderResponse(Long orderItemId, String orderItemName, int orderPrice, int orderCount, LocalDateTime orderDate, int orderItemStockQuantity, OrderStatus orderStatus) {
         this.orderItemId = orderItemId;
         this.orderItemName = orderItemName;
         this.orderPrice = orderPrice;
+        this.orderCount = orderCount;
         this.orderDate = orderDate;
         this.orderItemStockQuantity = orderItemStockQuantity;
+        this.orderStatus = orderStatus;
     }
 }
