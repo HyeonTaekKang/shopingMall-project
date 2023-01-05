b8c6ae1a0ea2a8588dc0cff6518e49e3a0d2d3e2
Kangteak <wjavmtngkr1@naver.com>
2023-01-05 20:21:04 +0900

    멤버변수인 orderPrice의 타입을 String -> int로 변경

--- src/main/java/com/homeshoping/homeshoping/entity/orderItem/OrderItem.java
+++ src/main/java/com/homeshoping/homeshoping/entity/orderItem/OrderItem.java
@@ -27,12 +27,12 @@ public class OrderItem {
     @JoinColumn(name = "item_id")
     private Item item;
 
-    private String orderPrice; // 주문 가격 ( 주문 당시의 상품 가격 )
+    private int orderPrice; // 주문 가격 ( 주문 당시의 상품 가격 )
 
     private int orderCount; // 주문 수량 ( 몇 개를 주문했는지 )
 
     @Builder
-    public OrderItem(Order order, Item item, String orderPrice, int orderCount) {
+    public OrderItem(Order order, Item item, int orderPrice, int orderCount) {
         this.order = order;
         this.item = item;
         this.orderPrice = orderPrice;
@@ -40,7 +40,7 @@ public class OrderItem {
     }
 
     // 생성메서드
-    public static OrderItem createOrderItem(Item item , String orderPrice , int orderCount){
+    public static OrderItem createOrderItem(Item item , int orderPrice , int orderCount){
         OrderItem orderItem = new OrderItem();
 
         // 연관관계 셋팅 ( orderItem - item  )
@@ -48,6 +48,9 @@ public class OrderItem {
 
         orderItem.orderPrice = orderPrice;
         orderItem.orderCount = orderCount;
+
+        // 주문한 수량만큼 item 의 재고량 감소
+        item.removeStockQuantity(orderCount);
 
         return orderItem;
     }
@@ -61,4 +64,5 @@ public class OrderItem {
     public void setOrder(Order order){
         this.order = order;
     }
+
 }
