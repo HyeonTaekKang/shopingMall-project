0eb2bba2a4d5164cc60bc297d9a217cfeba5d527
Kangteak <wjavmtngkr1@naver.com>
2023-01-07 00:27:54 +0900

    DeliveryRepository생성.

---
+++ src/main/java/com/homeshoping/homeshoping/repository/delivery/DeliveryRepository.java
@@ -0,0 +1,7 @@
+package com.homeshoping.homeshoping.repository.delivery;
+
+import com.homeshoping.homeshoping.entity.delivery.Delivery;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DeliveryRepository extends JpaRepository<Delivery,Long> {
+}
