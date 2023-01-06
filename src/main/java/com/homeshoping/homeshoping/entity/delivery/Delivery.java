77cfe7d4676de76214756eb5e8d38dd005554aef
Kangteak <wjavmtngkr1@naver.com>
2023-01-07 00:15:57 +0900

    Order와 @OneToOne 연관관계 맵핑하고, Delivery객체 생성 메서드를 만들었고 , Order와의 연관관계 맵핑을 위한 연관관계 편의 메서드 생성.

--- src/main/java/com/homeshoping/homeshoping/entity/delivery/Delivery.java
+++ src/main/java/com/homeshoping/homeshoping/entity/delivery/Delivery.java
@@ -1,12 +1,16 @@
 package com.homeshoping.homeshoping.entity.delivery;
 
-import com.homeshoping.homeshoping.entity.member.Address;
+import com.homeshoping.homeshoping.entity.address.Address;
+import com.homeshoping.homeshoping.entity.order.Order;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class Delivery {
 
     @Id
@@ -19,4 +23,35 @@ public class Delivery {
 
     @Embedded
     private Address address;
+
+    @OneToOne( mappedBy = "delivery", fetch = FetchType.LAZY)
+    private Order order;
+
+    @Builder
+    public Delivery(Long id, DeliveryStatus deliveryStatus, Address address, Order order) {
+        this.id = id;
+        this.deliveryStatus = deliveryStatus;
+        this.address = address;
+        this.order = order;
+    }
+
+    // 배달 정보 생성 메서드
+    public static Delivery createDelivery(Address address){
+        Delivery delivery = new Delivery();
+
+        delivery.deliveryStatus = DeliveryStatus.READY;
+
+        delivery.address = Address.builder()
+                .city(address.getCity())
+                .street(address.getStreet())
+                .zipcode(address.getZipcode())
+                .build();
+
+        return delivery;
+    }
+
+    // Order와 연관관계 맺기
+    public void setOrder(Order order){
+        this.order = order;
+    }
 }
