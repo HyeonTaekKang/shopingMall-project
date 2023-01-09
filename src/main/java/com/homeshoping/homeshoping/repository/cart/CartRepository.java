0f53c197c7cf569c1aa88c7770e4de7ca62a8dbe
Kangteak <wjavmtngkr1@naver.com>
2023-01-10 01:42:41 +0900

    Querydsl이 들어있는 CartRepositoryCustom 인터페이스를 기존 리포지토리에 추가

--- src/main/java/com/homeshoping/homeshoping/repository/cart/CartRepository.java
+++ src/main/java/com/homeshoping/homeshoping/repository/cart/CartRepository.java
@@ -3,5 +3,5 @@ package com.homeshoping.homeshoping.repository.cart;
 import com.homeshoping.homeshoping.entity.cart.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CartRepository extends JpaRepository<Cart, Long> {
+public interface CartRepository extends JpaRepository<Cart, Long> , CartRepositoryCustom {
 }
