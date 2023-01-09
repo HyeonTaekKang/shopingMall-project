7bf385c212bb9e715c8013692898ccb4b7666f34
Kangteak <wjavmtngkr1@naver.com>
2023-01-10 01:45:22 +0900

    "엔티티 -> DTO로 바꾸기 위해서" 기존의 생성자를 오버로딩한 새로운 생성자를 만들었음.

--- src/main/java/com/homeshoping/homeshoping/response/cart/CartResponse.java
+++ src/main/java/com/homeshoping/homeshoping/response/cart/CartResponse.java
@@ -1,18 +1,21 @@
 package com.homeshoping.homeshoping.response.cart;
 
+import com.homeshoping.homeshoping.entity.cart.Cart;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class CartResponse {
 
-    private String seller; // 회사명 or 판매자 이름
+    private String seller; // 아이템을 판매하는 회사명 or 판매자 이름
 
-    private String itemName; // 아이템명
+    private String itemName; // 카드에 담은 아이템명
 
-    private int itemCount; // 아이템 수량
+    private int itemCount; // 카드에 담은 아이템의 수량
 
-    private int itemPrice; // 아이템 가격
+    private int itemPrice; // 카드에 담은 아이템의 가격
 
     @Builder
     public CartResponse(String seller, String itemName, int itemCount, int itemPrice) {
@@ -20,5 +23,14 @@ public class CartResponse {
         this.itemName = itemName;
         this.itemCount = itemCount;
         this.itemPrice = itemPrice;
+    }
+
+    // 생성자 오버로딩 ( 엔티티 -> DTO )
+    @Builder
+    public CartResponse(Cart cart) {
+        this.seller = cart.getMember().getName();
+        this.itemName = cart.getItem().getName();
+        this.itemCount = cart.getItemCount();
+        this.itemPrice = cart.getItem().getPrice();
     }
 }
