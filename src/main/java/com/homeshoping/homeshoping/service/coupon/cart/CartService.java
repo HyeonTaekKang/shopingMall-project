551d15d275f11efb74a4fdcc3b53f7889cdc4b91
Kangteak <wjavmtngkr1@naver.com>
2023-01-12 02:05:41 +0900

    cart에 담긴 아이템들을 가져오는 메서드인 "getItemsInCart" 메서드를 제작했음.

--- src/main/java/com/homeshoping/homeshoping/service/coupon/cart/CartService.java
+++ src/main/java/com/homeshoping/homeshoping/service/coupon/cart/CartService.java
@@ -1,4 +1,4 @@
-package com.homeshoping.homeshoping.service.cart;
+package com.homeshoping.homeshoping.service.coupon.cart;
 
 import com.homeshoping.homeshoping.Exception.ItemNotFound;
 import com.homeshoping.homeshoping.Exception.MemberNotFound;
@@ -9,10 +9,16 @@ import com.homeshoping.homeshoping.repository.Item.ItemRepository;
 import com.homeshoping.homeshoping.repository.cart.CartRepository;
 import com.homeshoping.homeshoping.repository.member.MemberRepository;
 import com.homeshoping.homeshoping.request.cart.CartCreate;
+import com.homeshoping.homeshoping.response.cart.CartResponse;
+import com.homeshoping.homeshoping.response.cart.ListResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
@@ -36,6 +42,18 @@ public class CartService {
         Cart cart = Cart.createCart(item, member , cartCreate.getItemCount());
 
         cartRepository.save(cart);
+    }
 
+    // cart 에 담긴 아이템들 가져오기
+    public ListResponse getItemsInCart(Long memberId){
+
+        // cart 에 담긴 아이템들을 리스트에 담아서 가져옴.
+        List<Cart> cartItems = cartRepository.getCartItems(memberId);
+
+        // 엔티티 -> 리스트<DTO>
+        List<CartResponse> cartItemsDTO = cartItems.stream().map(i -> new CartResponse(i)).collect(Collectors.toList());
+
+        // 리스트<DTO> 를 객체에 담아서 리턴.
+        return new ListResponse(cartItemsDTO);
     }
 }
