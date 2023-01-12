d5df94704b182de431e4a57b23195918a5a520dc
Kangteak <wjavmtngkr1@naver.com>
2023-01-13 00:31:15 +0900

    MemberCouponRepository 제작.

---
+++ src/main/java/com/homeshoping/homeshoping/repository/memberCoupon/MemberCouponRepository.java
@@ -0,0 +1,7 @@
+package com.homeshoping.homeshoping.repository.memberCoupon;
+
+import com.homeshoping.homeshoping.entity.memberCoupon.MemberCoupon;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberCouponRepository extends JpaRepository<MemberCoupon, Long> {
+}
