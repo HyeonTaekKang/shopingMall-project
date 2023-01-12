25ea703356351e61016dfd0920c3686cd4d1ba83
Kangteak <wjavmtngkr1@naver.com>
2023-01-13 00:43:04 +0900

    쿠폰을 발급받게 해주는 메서드인 downloadCoupon() 이라는 메서드를 제작함.

--- src/main/java/com/homeshoping/homeshoping/service/member/MemberService.java
+++ src/main/java/com/homeshoping/homeshoping/service/member/MemberService.java
@@ -1,8 +1,12 @@
 package com.homeshoping.homeshoping.service.member;
 
 import com.homeshoping.homeshoping.Exception.MemberNotFound;
+import com.homeshoping.homeshoping.entity.coupon.Coupon;
 import com.homeshoping.homeshoping.entity.member.Member;
+import com.homeshoping.homeshoping.entity.memberCoupon.MemberCoupon;
+import com.homeshoping.homeshoping.repository.coupon.CouponRepository;
 import com.homeshoping.homeshoping.repository.member.MemberRepository;
+import com.homeshoping.homeshoping.repository.memberCoupon.MemberCouponRepository;
 import com.homeshoping.homeshoping.request.member.MemberCreate;
 import com.homeshoping.homeshoping.response.member.MemberResponse;
 import lombok.RequiredArgsConstructor;
@@ -13,11 +17,32 @@ import org.springframework.stereotype.Service;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final CouponRepository couponRepository;
+    private final MemberCouponRepository memberCouponRepository;
 
     // 회원가입
     public void join(MemberCreate memberCreate){
         memberRepository.save(new Member(memberCreate));
     }
+
+
+    // 쿠폰 발급받기
+    public void downloadCoupon(Long memberId , Long couponCode){
+
+        // 쿠폰을 발급받을 멤버 찾아오기
+        Member member = memberRepository.findById(memberId).orElseThrow(MemberNotFound::new);
+
+        // 매개변수로 받아온 couponCode로 쿠폰 찾아오기
+        Coupon coupon = couponRepository.findByCode(couponCode);
+
+        MemberCoupon memberCoupon = MemberCoupon.createMemberCoupon(member, coupon);
+
+        memberCouponRepository.save(memberCoupon);
+
+    }
+
+    // 쿠폰 사용
+
 
 
     // 회원정보 가져오기
