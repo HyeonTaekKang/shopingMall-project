48e6d347bb91cf7f79fca9fdff918e78a9708abb
Kangteak <wjavmtngkr1@naver.com>
2023-01-15 10:17:32 +0900

    회원이 주문을 할 때 "쿠폰" 을 사용했을 경우 일어날 로직들을 테스트하기 위한 테스트코드를 작성함.

--- src/test/java/com/homeshoping/homeshoping/service/order/OrderServiceTest.java
+++ src/test/java/com/homeshoping/homeshoping/service/order/OrderServiceTest.java
@@ -1,19 +1,25 @@
 package com.homeshoping.homeshoping.service.order;
 
-import com.homeshoping.homeshoping.entity.Item.Album;
+import com.homeshoping.homeshoping.entity.Item.Category.Album;
+import com.homeshoping.homeshoping.entity.Item.Item;
 import com.homeshoping.homeshoping.entity.address.Address;
+import com.homeshoping.homeshoping.entity.coupon.Coupon;
 import com.homeshoping.homeshoping.entity.delivery.Delivery;
+import com.homeshoping.homeshoping.entity.member.Member;
 import com.homeshoping.homeshoping.entity.order.OrderStatus;
 import com.homeshoping.homeshoping.repository.Item.ItemRepository;
+import com.homeshoping.homeshoping.repository.coupon.CouponRepository;
 import com.homeshoping.homeshoping.repository.delivery.DeliveryRepository;
 import com.homeshoping.homeshoping.repository.member.MemberRepository;
 import com.homeshoping.homeshoping.repository.order.OrderRepository;
 import com.homeshoping.homeshoping.request.Item.ItemCreate;
+import com.homeshoping.homeshoping.request.coupon.CouponCreate;
 import com.homeshoping.homeshoping.request.member.AddressCreate;
 import com.homeshoping.homeshoping.request.member.MemberCreate;
 import com.homeshoping.homeshoping.request.order.OrderCreate;
 import com.homeshoping.homeshoping.response.order.OrderResponse;
 import com.homeshoping.homeshoping.service.Item.ItemService;
+import com.homeshoping.homeshoping.service.coupon.CouponService;
 import com.homeshoping.homeshoping.service.member.MemberService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -42,6 +48,9 @@ class OrderServiceTest {
     ItemService itemService;
 
     @Autowired
+    CouponService couponService;
+
+    @Autowired
     MemberRepository memberRepository;
 
     @Autowired
@@ -52,6 +61,9 @@ class OrderServiceTest {
 
     @Autowired
     DeliveryRepository deliveryRepository;
+
+    @Autowired
+    CouponRepository couponRepository;
 
     @PersistenceContext
     EntityManager em;
@@ -71,14 +83,15 @@ class OrderServiceTest {
     void createOrderTest(){
         // given
         // member 생성
-        MemberCreate newMember = createNewMember();
+        createNewMember();
 
         // item 생성 ( 앨범 )
-        ItemCreate newItem = createNewItem();
+        createNewItem();
 
         // delivery 생성
         createNewDelivery();
 
+        // when
         // 새로운 order 셋팅
         OrderCreate newOrder = createNewOrder();
 
@@ -89,16 +102,52 @@ class OrderServiceTest {
     }
 
     @Test
+    @DisplayName("order 생성 테스트 ( with coupon )")
+    @Rollback(value = false)
+    @Transactional
+    void createOrderWithCouponTest(){
+        // given
+        // member 생성
+        Long newMemberId = createNewMember();
+
+        // item 생성 ( 앨범 )
+        Long newItemId = createNewItem();
+
+        // delivery 생성
+        Long newDeliveryId = createNewDelivery();
+
+        // 쿠폰 셋팅 ( 쿠폰 코드 리턴 )
+        Long couponCode = createNewCoupon();
+
+        // Order 정보 셋팅
+        OrderCreate orderCreate = OrderCreate.builder()
+                .memberId(newMemberId)
+                .itemId(newItemId)
+                .deliveryId(newDeliveryId)
+                .orderCount(10)
+                .couponCode(couponCode)
+                .build();
+
+        // when
+        // 위에서 만든 Order 정보로 새로운 order 생성
+        orderService.create(orderCreate);
+
+        // then
+        // ( member , item(album) , delivery , coupon , member_coupon , orders , order_item )
+
+    }
+
+    @Test
     @DisplayName("회원이 주문한 '주문 정보 가져오기 테스트' ")
     @Rollback(value = false)
     @Transactional
     void getOrderTest(){
 
         // 새로운 member 생성
-        MemberCreate newMember = createNewMember();
+        createNewMember();
 
         // 새로운 item 생성 ( 앨범 )
-        ItemCreate newItem = createNewItem();
+        createNewItem();
 
         // 새로운 order 셋팅
         OrderCreate newOrder = createNewOrder();
@@ -121,25 +170,26 @@ class OrderServiceTest {
 
 
 
-
+    // =====================================================================================================
 
     // 새로운 배달정보 생성 메서드
-    private void createNewDelivery(){
+    private Long createNewDelivery(){
         Address address = Address.builder()
                 .city("송파구")
                 .street("석촌동")
                 .zipcode("1234")
                 .build();
 
-        Delivery delivery = Delivery.createDelivery(address);
+        Delivery newDelivery = Delivery.createDelivery(address);
 
-        deliveryRepository.save(delivery);
+        deliveryRepository.save(newDelivery);
+        return newDelivery.getId();
     }
 
 
 
     // 새로운 회원생성 메서드
-    private MemberCreate createNewMember(){
+    private Long createNewMember(){
         AddressCreate addressCreate = AddressCreate.builder()
                 .city("송파구")
                 .street("석촌동")
@@ -153,12 +203,15 @@ class OrderServiceTest {
                 .addressCreate(addressCreate)
                 .build();
 
-        memberService.join(memberCreate);
-        return memberCreate;
+        // 새로운 멤버 생성
+        Member newMember = Member.createMember(memberCreate);
+
+        memberRepository.save(newMember);
+        return newMember.getId();
     }
 
     // 새로운 아이템( 앨범 ) 생성 메서드
-    private ItemCreate createNewItem(){
+    private Long createNewItem(){
         // 제품 한개 등록 ( 앨범 )
         Album album = Album.builder()
                 .artist("에스파")
@@ -168,13 +221,16 @@ class OrderServiceTest {
                 .name("savage")
                 .price(10000)
                 .stockQuantity(10000)
-                .date(LocalDateTime.now())
                 .itemType("Album")
                 .album(album)
                 .build();
 
-        itemService.itemRegistration(itemCreate);
-        return itemCreate;
+        // 아이템 생성
+        Item newItem = Item.createItem(itemCreate);
+
+        // 아이템 저장
+        itemRepository.save(newItem);
+        return newItem.getId();
     }
 
     // 새로운 주문 생성 메서드
@@ -188,6 +244,34 @@ class OrderServiceTest {
                 .build();
 
         return orderCreate;
+    }
+
+    // 쿠폰 생성 메서드
+    private Long createNewCoupon(){
+
+        // 쿠폰 정보 셋팅
+        CouponCreate couponCreate = CouponCreate.builder()
+                .name("설날기념 10% 할인 쿠폰")
+                .description("설날 기념으로 전상품 10%할인!")
+                .discountPrice(1000)
+                .createdAt(LocalDateTime.now())
+                .expiredAt(LocalDateTime.now().plusDays(20))
+                .build();
+
+        // 위에서 만든 쿠폰 정보로 쿠폰을 생성하고 , 만들어진 쿠폰의 쿠폰코드 리턴
+        Long couponCode = couponService.createCoupon(couponCreate);
+
+        return couponCode;
+    }
+
+    // 새로운 쿠폰 생성
+    public Long createCoupon(CouponCreate couponCreate){
+
+        // DTO -> entity
+        Coupon coupon = Coupon.createCoupon(couponCreate);
+
+        couponRepository.save(coupon);
+        return coupon.getCode();
     }
 
 
