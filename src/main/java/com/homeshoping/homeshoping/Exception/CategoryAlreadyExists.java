4dea09bdfd6cf0d53e984311c9d9127c3b9ca1da
Kangteak <wjavmtngkr1@naver.com>
2023-01-18 01:58:55 +0900

    이미 존재하는 카테고리를 생성할려고 하면 던져지는 exception인 CategoryAlreadyExists

---
+++ src/main/java/com/homeshoping/homeshoping/Exception/CategoryAlreadyExists.java
@@ -0,0 +1,12 @@
+package com.homeshoping.homeshoping.Exception;
+
+public class CategoryAlreadyExists extends MainException {
+    private final static String MESSAGE = "이미 존재하는 카테고리입니다.";
+
+    public CategoryAlreadyExists(){super(MESSAGE);}
+
+    @Override
+    public int getStatusCode() {
+        return 400;
+    }
+}
