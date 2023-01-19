c45f99bbe1765f960e4f5ea4f1b2433510ba599d
Kangteak <wjavmtngkr1@naver.com>
2023-01-20 05:58:51 +0900

    아이템의 대분류별로 제품을 가져오는 getAllItemByCategoryBranch() 메서드 생성 , 아이템의 소분류별로 제품을 가져오는 getAllItemByCategoryName() 메서드 생성.

--- src/main/java/com/homeshoping/homeshoping/repository/Item/ItemRepositoryImpl.java
+++ src/main/java/com/homeshoping/homeshoping/repository/Item/ItemRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.homeshoping.homeshoping.repository.Item;
 
 import com.homeshoping.homeshoping.entity.Item.Item;
 
+import com.homeshoping.homeshoping.entity.category.QCategory;
 import com.homeshoping.homeshoping.request.Item.ItemCreate;
 import com.homeshoping.homeshoping.request.Item.ItemSearch;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -11,6 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 import static com.homeshoping.homeshoping.entity.Item.QItem.*;
+import static com.homeshoping.homeshoping.entity.category.QCategory.*;
 
 @RequiredArgsConstructor
 public class ItemRepositoryImpl implements ItemRepositoryCustom {
@@ -24,6 +26,28 @@ public class ItemRepositoryImpl implements ItemRepositoryCustom {
                 .selectFrom(item)
                 .offset(itemSearch.getOffset())
                 .limit(itemSearch.getLimit())
+                .orderBy(item.createdAt.desc())
+                .fetch();
+    }
+
+    // 대분류 카테고리별로 상품 가져오기
+    @Override
+    public List<Item> getAllItemByCategoryBranch(String categoryBranch){
+        return jpaQueryFactory
+                .selectFrom(item)
+                .join(item.category, category).fetchJoin()
+                .where(category.branch.eq(categoryBranch))
+                .orderBy(item.createdAt.desc())
+                .fetch();
+    }
+
+    // 소분류 카테고리별로 상품 가져오기
+    @Override
+    public List<Item> getAllItemByCategoryName(String categoryName){
+        return jpaQueryFactory
+                .selectFrom(item)
+                .join(item.category, category).fetchJoin()
+                .where(category.name.eq(categoryName))
                 .orderBy(item.createdAt.desc())
                 .fetch();
     }
