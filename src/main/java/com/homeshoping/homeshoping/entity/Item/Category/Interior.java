0733967ff5de008b73d6693dfe90ac3e1261adc4
Kangteak <wjavmtngkr1@naver.com>
2023-01-20 05:55:35 +0900

    의류 쇼핑몰을 만들기로 결정됐기 때문에 다른 아이템들은 모두 삭제함.

--- src/main/java/com/homeshoping/homeshoping/entity/Item/Category/Interior.java
+++
@@ -1,4 +0,0 @@
-package com.homeshoping.homeshoping.entity.Item.Category;
-
-public class Interior {
-}
--- src/main/java/com/homeshoping/homeshoping/entity/Item/Category/Stationery.java
+++
@@ -1,4 +0,0 @@
-package com.homeshoping.homeshoping.entity.Item.Category;
-
-public class Stationery {  // 문구류
-}
