eaf7414394561edaa88e14bcacc0c6ec4143a014
Kangteak <wjavmtngkr1@naver.com>
2023-01-23 22:11:39 +0900

    Category -> ItemCategory

--- src/main/java/com/homeshoping/homeshoping/repository/itemCategory/ItemItemCategoryRepository.java
+++ src/main/java/com/homeshoping/homeshoping/repository/itemCategory/ItemItemCategoryRepository.java
@@ -1,23 +1,19 @@
-package com.homeshoping.homeshoping.repository.category;
+package com.homeshoping.homeshoping.repository.itemCategory;
 
 
-import com.homeshoping.homeshoping.entity.category.Category;
-import lombok.RequiredArgsConstructor;
+import com.homeshoping.homeshoping.entity.ItemCategory.ItemCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import java.util.List;
 import java.util.Optional;
 
 
-public interface CategoryRepository extends JpaRepository<Category , Long> , CategoryRepositoryCustom {
+public interface ItemItemCategoryRepository extends JpaRepository<ItemCategory, Long> , ItemCategoryRepositoryCustom {
 
 //    // 카테고리의 이름으로 해당 카테고리를 찾아오기
 //    Optional<Category> findByName (String name);
 //
     // 카테고리의 branch와 이름으로 해당 카테고리를 찾아오기
-    Optional<Category> findByBranchAndName (String branch, String name);
+    Optional<ItemCategory> findByBranchAndName (String branch, String name);
 
 //    // 해당 카테고리가 존재하는지 branch와 name으로 검색하여 true or false를 반환.
 //    Boolean existsByBranchAndName(String branch, String name);
