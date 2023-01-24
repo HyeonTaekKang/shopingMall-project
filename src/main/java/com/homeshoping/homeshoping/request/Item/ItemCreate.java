011727c223d6212cc3d78919dec2883146c20c26
Kangteak <wjavmtngkr1@naver.com>
2023-01-24 09:57:33 +0900

    상품 정보를 받기 위한 필드인 'itemInfoCreate' 필드 생성 , 여러 상품 옵션을 받기 위한 필드인 'itemOptionCreateList' 생성 , 상품 카테고리 정보를 받기 위한 itemCategoryCreate 필드 생성

--- src/main/java/com/homeshoping/homeshoping/request/Item/ItemCreate.java
+++ src/main/java/com/homeshoping/homeshoping/request/Item/ItemCreate.java
@@ -1,9 +1,10 @@
 package com.homeshoping.homeshoping.request.Item;
 
-
-import com.homeshoping.homeshoping.entity.Item.Category.Album;
-import com.homeshoping.homeshoping.entity.Item.Category.Food;
-
+import com.homeshoping.homeshoping.entity.ItemCategory.ItemCategory;
+import com.homeshoping.homeshoping.entity.itemInfo.ItemInfo;
+import com.homeshoping.homeshoping.request.itemCategory.ItemCategoryCreate;
+import com.homeshoping.homeshoping.request.itemInfo.ItemInfoCreate;
+import com.homeshoping.homeshoping.request.itemOption.ItemOptionCreate;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,8 @@ import lombok.ToString;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @ToString
@@ -26,33 +29,59 @@ public class ItemCreate {
     @NotNull(message = "상품의 가격을 입력해주세요!")
     private int price;       // 상품가격
 
+    private ItemInfoCreate itemInfoCreate; // 상품 정보
+
+    private List<ItemOptionCreate> itemOptionCreateList = new ArrayList<>(); // 아이템 옵션
+
+    private ItemCategoryCreate itemCategoryCreate; // 상품 카테고리
+
     @NotNull(message = "상품의 재고를 입력해주세요!")
     private int stockQuantity;   // 상품재고
 
-    @NotBlank(message = "상품의 타입을 알려주세요!")
-    private String itemType;  // 상품 타입
+    private String comment; // 판매자 코멘트
 
     private LocalDateTime createdAt; // 상품 등록 날짜
 
     private LocalDateTime modifiedAt; // 상품 변경일
 
-    // Album
-    private Album album;
-
-    // Food
-    private Food food;
-
     @Builder
-    public ItemCreate(Long id, String name, int price, int stockQuantity, String itemType, LocalDateTime createdAt, LocalDateTime modifiedAt, Album album, Food food) {
+    public ItemCreate(Long id, String name, int price, ItemInfoCreate itemInfoCreate, List<ItemOptionCreate> itemOptionCreateList, ItemCategoryCreate itemCategoryCreate, int stockQuantity, String comment, LocalDateTime createdAt, LocalDateTime modifiedAt) {
         this.id = id;
         this.name = name;
         this.price = price;
+        this.itemInfoCreate = itemInfoCreate;
+        this.itemOptionCreateList = itemOptionCreateList;
+        this.itemCategoryCreate = itemCategoryCreate;
         this.stockQuantity = stockQuantity;
-        this.itemType = itemType;
-        this.createdAt = LocalDateTime.now(); // 생성날짜를 객체가 생성된 지금으로.
+        this.comment = comment;
+        this.createdAt = createdAt;
         this.modifiedAt = modifiedAt;
-        this.album = album;
-        this.food = food;
+    }
+
+    // DTO -> entity
+    public ItemInfo toEntity(ItemInfoCreate itemInfoCreate){
+        return ItemInfo.builder()
+                .manufactureCountry(itemInfoCreate.getManufactureCountry())
+                .material(itemInfoCreate.getMaterial())
+                .color(itemInfoCreate.getColor())
+                .size(itemInfoCreate.getSize())
+                .maker(itemInfoCreate.getMaker())
+                .washingMethod(itemInfoCreate.getWashingMethod())
+                .yearAndMonthofManufacture(itemInfoCreate.getYearAndMonthofManufacture())
+                .manager(itemInfoCreate.getManager())
+                .qualityStandard(itemInfoCreate.getQualityStandard())
+                .build();
+    }
+
+    public ItemCategory toEntity(ItemCategoryCreate smallItemCategoryCreate){
+        return ItemCategory.builder()
+                .branch(smallItemCategoryCreate.getBranch())
+                .name(smallItemCategoryCreate.getName())
+                .parentItemCategory(ItemCategory.builder()
+                        .branch(smallItemCategoryCreate.getParentItemCategory().getBranch())
+                        .name(smallItemCategoryCreate.getParentItemCategory().getName())
+                        .build())
+                .build();
     }
 
     // == 비지니스 메서드 ==
