5002c0cf8a466467d30efb16173d5927153659eb
Kangteak <wjavmtngkr1@naver.com>
2023-02-10 23:51:36 +0900

    ItemInfo를 수정하기 위한 메서드인 editItemInfo() 를 제작함. ( ItemInfoEdit 을 매개변수로 받음 )

--- src/main/java/com/homeshoping/homeshoping/entity/itemInfo/ItemInfo.java
+++ src/main/java/com/homeshoping/homeshoping/entity/itemInfo/ItemInfo.java
@@ -4,6 +4,7 @@ package com.homeshoping.homeshoping.entity.itemInfo;
 import com.homeshoping.homeshoping.entity.Item.Item;
 import com.homeshoping.homeshoping.request.Item.ItemCreate;
 import com.homeshoping.homeshoping.request.itemInfo.ItemInfoCreate;
+import com.homeshoping.homeshoping.request.itemInfo.ItemInfoEdit;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -22,7 +23,7 @@ public class ItemInfo {
     @Column(name = "itemInfo_id")
     private Long id;
 
-    @OneToOne(mappedBy = "itemInfo",fetch = FetchType.LAZY)
+    @OneToOne(mappedBy = "itemInfo",fetch = FetchType.LAZY , cascade = CascadeType.ALL)
     private Item item;
 
     private String manufactureCountry; // 제조국 ( 반드시 필요! )
@@ -60,8 +61,23 @@ public class ItemInfo {
         return itemInfo;
     }
 
+    // ItemInfo 수정 메서드
+    public void editItemInfo(ItemInfoEdit itemInfoEdit){
+        this.manufactureCountry = itemInfoEdit.getManufactureCountry();
+        this.material = itemInfoEdit.getMaterial();
+        this.color = itemInfoEdit.getColor();
+        this.size = itemInfoEdit.getSize();
+        this.maker = itemInfoEdit.getMaker();
+        this.washingMethod = itemInfoEdit.getWashingMethod();
+        this.yearAndMonthofManufacture = itemInfoEdit.getYearAndMonthofManufacture();
+        this.manager = itemInfoEdit.getManager();
+        this.qualityStandard = itemInfoEdit.getQualityStandard();
+    }
+
     @Builder
-    public ItemInfo(String manufactureCountry, String material, String color, String size, String maker, String washingMethod, LocalDate yearAndMonthofManufacture, String manager, String qualityStandard) {
+    public ItemInfo(Long id, Item item, String manufactureCountry, String material, String color, String size, String maker, String washingMethod, LocalDate yearAndMonthofManufacture, String manager, String qualityStandard) {
+        this.id = id;
+        this.item = item;
         this.manufactureCountry = manufactureCountry;
         this.material = material;
         this.color = color;
