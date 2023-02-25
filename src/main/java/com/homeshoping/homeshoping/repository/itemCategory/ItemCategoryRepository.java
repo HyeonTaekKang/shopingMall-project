84bdd9ab53d90cc287ae96a4c68a41b368965132
Kangteak <wjavmtngkr1@naver.com>
2023-02-25 21:30:02 +0900

    대분류가 존재하는지 판단하는 메서드인 existsByBranch() 생성

--- src/main/java/com/homeshoping/homeshoping/repository/itemCategory/ItemCategoryRepository.java
+++ src/main/java/com/homeshoping/homeshoping/repository/itemCategory/ItemCategoryRepository.java
@@ -4,14 +4,18 @@ package com.homeshoping.homeshoping.repository.itemCategory;
 import com.homeshoping.homeshoping.entity.ItemCategory.ItemCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 
-public interface ItemItemCategoryRepository extends JpaRepository<ItemCategory, Long> , ItemCategoryRepositoryCustom {
+public interface ItemCategoryRepository extends JpaRepository<ItemCategory, Long> , ItemCategoryRepositoryCustom {
 
-//    // 카테고리의 이름으로 해당 카테고리를 찾아오기
-//    Optional<Category> findByName (String name);
-//
+    // 카테고리의 branch로 카테고리를 찾아오기
+    Optional<ItemCategory> findByBranch(String branch);
+
+    // 카테고리의 name으로 카테고리를 찾아오기
+//    Optional<ItemCategory> findByName (String name);
+
     // 카테고리의 branch와 이름으로 해당 카테고리를 찾아오기
     Optional<ItemCategory> findByBranchAndName (String branch, String name);
 
@@ -22,5 +26,8 @@ public interface ItemItemCategoryRepository extends JpaRepository<ItemCategory, 
 //    Optional<Category> findByBranchAndCode (String branch, String code);
 
     // 카테고리의 이름으로 해당 카테고리가 존재하는지 판단하는 메서드
-    Boolean existsByBranchAndName (String branch , String name);
+    Boolean existsByBranchAndName (String branch, String name);
+
+    // 해당 대분류가 존재하는지 판단하는 메서드 ( 매개변수로 대분류 이름을 받음 )
+    Boolean existsByBranch (String branch);
 }
